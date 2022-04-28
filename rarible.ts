6282af5d70f3d3da08d880813adc88a58d2a04dc
Phyllomedusa <matthew.vladimirovich@gmail.com>
2022-04-28 16:43:56 +0300

    fix

--- rarible.ts
+++ rarible.ts
@@ -2,7 +2,6 @@ import DiscordJS, { Intents } from 'discord.js'
 import WOKCommands from 'wokcommands'
 import path from 'path'
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
 dotenv.config()
 
 const client = new DiscordJS.Client({
@@ -13,7 +12,6 @@ client.on('ready',async () => {
     console.log('bot ready')
     new WOKCommands(client, {
         commandDir: path.join(__dirname,'commands'),
-        //featureDir: path.join(__dirname,'features'),
         typeScript: true,
         testServers:['965209143801704480'],
         mongoUri:process.env.MONGO_URI,
